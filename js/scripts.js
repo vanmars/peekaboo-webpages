6e221452fef0be8a01c447a8bd03184679162224
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-11 09:51:52 -0700

    Add clickable cat image and text
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com> and Joseph Karnafel <josephkarnafel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
+
+  $(".clickable2").click(function(){
+    $("#cat-showing").fadeToggle();
+    $("#cat-hidden").fadeToggle();
+  });
+
 });
